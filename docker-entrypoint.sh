797f1fea1cdd81f014e427306651f20cb8c319cb
Nitin Sharma <nitinjbn@gmail.com>
2026-07-29 10:14:20 +0530

    docker changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$SERVICE_TYPE" = "WORKER" ]; then
   echo "Starting BullMQ Worker..."
-  exec node dist/workers/index.js
+  exec node dist/infrastructure/background-jobs/worker.js
 else
   echo "Starting API..."
   exec node dist/server.production.js
